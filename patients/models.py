0536c420fca2872c50b6e3fc4ea8280eaffb6dd6
nancymwende <nancymwende860@gmail.com>
2026-03-05 10:26:19 +0300

    create patient models

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Patient(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    gender = models.CharField(max_length=10)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
