a3aabb3566a76bd1b81d042b808fe4bae0883195
nancymwende <nancymwende860@gmail.com>
2026-03-06 09:08:31 +0300

    modify models and run migrations

--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
