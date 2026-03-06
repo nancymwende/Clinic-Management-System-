0ab29dc38fc62230fb49b2d21eb6081a3e3bdfa1
nancymwende <nancymwende860@gmail.com>
2026-03-06 09:15:04 +0300

    create appointment models

--- appointments/models.py
+++ appointments/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from patients.models import Patient
+from doctors.models import Doctor
 
-# Create your models here.
+
+class Appointment(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled'),
+    ]
+
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    appointment_date = models.DateTimeField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Pending')
+
+    def __str__(self):
+        return f"Appointment: {self.patient} with {self.doctor}"
