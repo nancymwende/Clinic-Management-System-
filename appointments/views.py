ab4fb2ce995ef61b6bbe120c3aae67fdc0e95ae6
nancymwende <nancymwende860@gmail.com>
2026-03-09 22:36:27 +0300

    modify views in appoinment folder

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .forms import AppointmentForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def book_appointment(request):
+    form = AppointmentForm()
+
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    return render(request, 'book_appointment.html', {'form': form})
